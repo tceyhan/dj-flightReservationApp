50373f4c1c06de212b7d6c524553745efef084ae
tceyhan <55tc155@gmail.com>
2022-06-20 21:50:29 +0300

    added  debug toolbar

--- main/settings.py
+++ main/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third party apps:
     'rest_framework',
-    'drf_yasg', 
+    'drf_yasg',
+    "debug_toolbar",  
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware", 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [ 
+    "127.0.0.1", 
+]
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # Three modules for swagger:
 from rest_framework import permissions
@@ -28,4 +28,5 @@ urlpatterns = [
     path("swagger(<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
